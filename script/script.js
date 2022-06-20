0235d3cf0737e46cab1e5ebe63fa6e7b5741cd7f
Alex Prc <pralig@yandex.ru>
2022-06-20 15:50:34 +0300

    сверстать попап и сделать скрипт

---
+++ script/script.js
@@ -0,0 +1,33 @@
+let popup = document.querySelector(".popup");
+
+let nameInput = document.querySelector(".popup__input-name");
+let jobInput = document.querySelector(".popup__input-job");
+
+let profileTitle = document.querySelector(".profile__title");
+let profileSubtitle = document.querySelector(".profile__subtitle");
+
+let editButton = document.querySelector(".profile__edit-button");
+let closedButton = document.querySelector(".popup__closed");
+
+let formElement = document.querySelector(".popup__form");
+
+function openPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function closedPopup() {
+  popup.classList.remove("popup_opened");
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  closedPopup();
+}
+
+editButton.addEventListener("click", openPopup);
+closedButton.addEventListener("click", closedPopup);
+formElement.addEventListener("submit", formSubmitHandler);
